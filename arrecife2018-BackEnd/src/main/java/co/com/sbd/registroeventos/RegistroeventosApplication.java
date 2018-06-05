5a3ecb8fbb66c6aa115f7aa196a3d48e9d24e96e
Diego Romero <dromero@outlook.com>
2018-06-04 20:57:57 -0500

    Primera versión

--- arrecife2018-BackEnd/src/main/java/co/com/sbd/registroeventos/RegistroeventosApplication.java
+++ arrecife2018-BackEnd/src/main/java/co/com/sbd/registroeventos/RegistroeventosApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RegistroeventosApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(RegistroeventosApplication.class, args);
+	
 	}
 }
